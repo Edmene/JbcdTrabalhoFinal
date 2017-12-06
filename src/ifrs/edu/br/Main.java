98f08ffb51f864cbfc3fcd4d956ed5546076bc39
Edmene <cristianedsonm.com>
2017-12-06 11:09:06 -0200

    Leitura da configuracao do banco por meio de arquivo conf..

--- src/ifrs/edu/br/Main.java
+++ src/ifrs/edu/br/Main.java
@@ -4,6 +4,10 @@ import ifrs.edu.br.negocio.Cliente;
 import ifrs.edu.br.negocio.Produto;
 import ifrs.edu.br.venda.Venda;
 import org.postgresql.ds.PGConnectionPoolDataSource;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
@@ -16,11 +20,49 @@ public class Main {
             PGConnectionPoolDataSource dataSource = new PGConnectionPoolDataSource();
             dataSource.setDefaultAutoCommit(false);
             ResultSet rs = null;
+            Path path = Paths.get("server.conf");
+            path.toFile().createNewFile();
+            File file = path.toFile();
+            Scanner scFile = new Scanner(file).useDelimiter(";");
+            String usuario = new String();
+            String senha = new String();
+            String nomeDb = new String();
+            String servidor = new String();
+            String porta = new String();
+            while (scFile.hasNext()){
+                String tmp = scFile.next();
+                tmp = tmp.replace(";","").trim();
+                if(tmp.contains("user=")){
+                    usuario=tmp.replace("user=", "");
+                }
+                if(tmp.contains("password=")){
+                    senha=tmp.replace("password=", "");
+                }
+                if(tmp.contains("db=")){
+                    nomeDb=tmp.replace("db=", "");
+                }
+                if(tmp.contains("server=")){
+                    servidor=tmp.replace("server=", "");
+                }
+                if(tmp.contains("port=")){
+                    porta=tmp.replace("port=", "");
+                }
+            }
+
+            /*
             dataSource.setDatabaseName("jdbc_work");
             dataSource.setServerName("localhost");
             dataSource.setPassword("postgres");
             dataSource.setPortNumber(5432);
             dataSource.setUser("postgres");
+            */
+
+            dataSource.setUser(usuario);
+            dataSource.setPassword(senha);
+            dataSource.setDatabaseName(nomeDb);
+            dataSource.setServerName(servidor);
+            dataSource.setPortNumber(Integer.valueOf(porta));
+
 
             boolean continuar = true;
             while(continuar){
